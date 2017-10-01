fde455affd032ad490d919a136fc764c562217e6
Alex Nichol <unixpickle@gmail.com>
2017-09-30 17:50:28 -0700

    fix place where name wasn't changed

--- mazenv/__init__.py
+++ mazenv/__init__.py
@@ -2,6 +2,6 @@
 Maze environments for Reinforcement Learning.
 """
 
-from .env import Env, HorizonEnv
+from .env import Env, HorizonLimit
 from .generate import prim
 from .maze import Maze, parse_2d_maze
